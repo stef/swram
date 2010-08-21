05a11488fc6fd087e1f38d2dd3f9148698668709
stef <stefan.marsiske@gmail.com>
2010-08-21 20:32:03 +0200

    [enh] some more fun

--- swarm.erl
+++ swarm.erl
@@ -7,9 +7,9 @@
                          random:uniform() * 10,
                          random:uniform() * 5},
                vitality=random:uniform(),
-               near={0,10},
+               avoid={0,10},
                comfort={10,100},
-               far={100,1000}}).
+               attract={100,1000}}).
 
 distance(A) when is_list(A) ->
     lists:map( fun(X) -> distance(X,A) end,
@@ -31,12 +31,28 @@ create_agents(N) ->
 gravity(A) when is_list(A) ->
     lists:map( fun(X) -> gravity(X,A) end,
          A).
-gravity(A,B) when is_record(A,agent) andalso is_record(B,agent) ->
-    1 / math:pow(distance(A,B),2);
 gravity(A,A) when is_record(A,agent) ->
     0;
+gravity(A,B) when is_record(A,agent) andalso is_record(B,agent) ->
+    1 / math:pow(distance(A,B),2);
 gravity(A,B) when is_record(A,agent) andalso is_list(B) ->
     lists:map(fun (X) -> gravity(A,X) end, B).
+
+in_range({Min, Max}, Distance)
+  when Min < Distance andalso Distance =< Max ->
+    true;
+in_range(_, _) ->
+    false.
+
+normal_vector(Agent,Bgent) ->
+    Distance=distance(Agent,Bgent),
+    {(element(1,Bgent#agent.pos) - element(1,Agent#agent.pos)) / Distance,
+     (element(2,Bgent#agent.pos) - element(2,Agent#agent.pos)) / Distance,
+     (element(3,Bgent#agent.pos) - element(3,Agent#agent.pos)) / Distance}.
+
+dump_positions(A) ->
+    lists:foreach(fun (X) ->
+                      io:format("~p ~p ~p~n", [element(1,X#agent.pos), element(2,X#agent.pos), element(3,X#agent.pos)]) end, A).
 
 test() ->
     test(100).
@@ -45,9 +61,16 @@ test(N) ->
     SWARM=create_agents(N),
     [ HEAD | TAIL ] = SWARM,
     [ HEAD1 | _ ] = TAIL,
-    io:format("Swarm: ~p~n",[SWARM]),
-    io:format("Distances: ~p~n",[swarm:distance(SWARM)]),
-    io:format("Distances: ~p~n",[swarm:distance(HEAD,TAIL)]),
-    io:format("Gravity: ~p~n",[swarm:gravity(HEAD,HEAD1)]),
-    io:format("Gravity: ~p~n",[swarm:gravity(HEAD,TAIL)]),
-    io:format("Gravity: ~p~n",[swarm:gravity(SWARM)]).
+    %io:format("Swarm: ~p~n",[SWARM]),
+    %io:format("Distances: ~p~n",[swarm:distance(SWARM)]),
+    %io:format("Distances: ~p~n",[swarm:distance(HEAD,TAIL)]),
+    %io:format("Gravity: ~p~n",[swarm:gravity(HEAD,HEAD1)]),
+    %io:format("Gravity: ~p~n",[swarm:gravity(HEAD,TAIL)]),
+    %io:format("Gravity: ~p~n",[swarm:gravity(SWARM)]),
+    %swarm:dump_positions(SWARM),
+    %io:format("inrange 1,5,2: ~p~n",[swarm:in_range({1,5},2)]),
+    %io:format("inrange 4,5,2: ~p~n",[swarm:in_range({4,5},2)]),
+    %io:format("inrange 4,5,7: ~p~n",[swarm:in_range({4,5},7)]),
+    %io:format("inrange 4,5,5: ~p~n",[swarm:in_range({4,5},5)]),
+    %io:format("inrange 4,5,4: ~p~n",[swarm:in_range({4,5},4)]),
+    io:format("normal Vector:  ~p~n",[swarm:normal_vector(HEAD,HEAD1)]).
